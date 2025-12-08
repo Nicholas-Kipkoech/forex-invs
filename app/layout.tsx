892b565b7100073916718072977b1d9029d4389d
Nicholas Kipkoech <nickey968@gmail.com>
2025-12-08 18:23:20 +0300

    remove google translate

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,26 +26,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-    script.async = true;
-    document.body.appendChild(script);
-
-    (window as any).googleTranslateElementInit = () => {
-      new (window as any).google.translate.TranslateElement(
-        {
-          pageLanguage: "en",
-          includedLanguages: "en,sw,fr,es,de", // customize languages
-          layout: (window as any).google.translate.TranslateElement.InlineLayout
-            .VERTICAL,
-        },
-        "google_translate_element"
-      );
-    };
-  }, []);
-
   return (
     <html lang="en">
       <body
@@ -53,7 +33,6 @@ export default function RootLayout({
       >
         {/* Translation Button */}
         <div
-          id="google_translate_element"
           style={{ position: "fixed", top: 10, right: 10, zIndex: 9999 }}
         ></div>
 
